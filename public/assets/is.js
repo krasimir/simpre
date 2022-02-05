140bad72711ae4886ac6ad6eb5828ec821ebd7a8
Krasimir Tsonev <krasimir@outset.ws>
2022-02-05 18:28:39 +0200

    Working on the positioning of the slides

--- public/assets/is.js
+++ public/assets/is.js
@@ -1,39 +1,4 @@
 (async function importSlide() {
-  function fit(w, h, node) {
-    const resize = () => {
-      const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-      const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-      const ratio = w / h;
-      // setting the right scale
-      let newW = vw;
-      let newH = newW / ratio;
-      if (newH > vh) {
-        newH = vh;
-        newW = ratio * newH;
-      }
-      const scale = newW / w;
-      let style = `display:block;transform-origin:top left;transform: scale(${scale}, ${scale});`;
-      node.setAttribute('style', style);
-      // setting the position
-      const wAfterScale = node.getBoundingClientRect().width;
-      const hAfterScale = node.getBoundingClientRect().height;
-      let top = false;
-      let left = false;
-      if (wAfterScale < vw - 10) {
-        top = 0;
-        left = (vw - wAfterScale) / 2;
-      }
-      if (hAfterScale < vh - 10) {
-        top = (vh - hAfterScale) / 2;
-      }
-      if (top !== false || left !== false) {
-        node.setAttribute('style', style + `position:absolute;left:${left}px;top:${top}px;`);
-      }
-    }
-    resize();
-    window.addEventListener('resize', resize);
-  }
-
   const options = document.currentScript.dataset;
   if (options.file) {
     const [err, text] = await load(options.file);
@@ -46,10 +11,6 @@
       codeNode.setAttribute('class', (options.class || ''));
       codeNode.innerHTML = Prism.highlight(text, Prism.languages.javascript, 'javascript');
       node.appendChild(codeNode);
-      setTimeout(() => {
-        const rect = node.getBoundingClientRect();
-        fit(rect.width, rect.height, node);
-      }, 1);
     }
     currentNode.parentNode.replaceChild(node, currentNode);
   }
---
+++ src/js/simpre.js
@@ -0,0 +1,91 @@
+const SLIDES_SELECTOR = 'section';
+const UP = ['ArrowUp', 38];
+const DOWN = ['ArrowDown', 40];
+const LEFT = ['ArrowLeft', 37];
+const RIGHT = ['ArrowRight', 39];
+const BACKSPACE = ['Backspace', 8];
+const FORWARD = [].concat(DOWN, RIGHT);
+const BACKWARD = [].concat(UP, LEFT, BACKSPACE);
+
+function init() {
+  const progressNode = document.querySelector('#progress');
+  const slides = document.querySelectorAll(SLIDES_SELECTOR);
+  const totalSlides = slides.length;
+  let current = -1;
+
+  showSlide(0);
+
+  document.querySelector('body').addEventListener('keyup', (e) => {
+    const key = e.key || e.keyCode;
+    if (FORWARD.includes(key)) { // ******************* forward
+      if (current + 1 < totalSlides) {
+        showSlide(current + 1);
+      }
+    } else if (BACKWARD.includes(key)) { // *********** backward
+      if (current - 1 >= 0) {
+        showSlide(current - 1);
+      }
+    }
+  });
+
+  function showSlide(idx) {
+    if (current >= 0) {
+      slides[current].style.opacity = 0;
+    }
+    current = idx;
+    slides[current].style.opacity = 1;
+    updateProgress();
+    setTimeout(() => {
+      fit(slides[current]);
+    }, 1);
+  }
+  function updateProgress() {
+    const percents = (current + 1) / totalSlides * 100;
+    progressNode.style.width = percents + '%';
+  }
+}
+
+function fit(node) {
+  if (node.scaled) return;
+  const rect = node.getBoundingClientRect();
+  const w =  rect.width
+  const h = rect.height;
+  const resize = () => {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    const ratio = w / h;
+    // setting the right scale
+    let newW = vw;
+    let newH = newW / ratio;
+    if (newH > vh) {
+      newH = vh;
+      newW = ratio * newH;
+    }
+    const scale = newW / w;
+    node.style.display = 'block';
+    node.style.transformOrigin = 'top left';
+    node.style.transform = `scale(${scale}, ${scale})`;
+    // setting the position
+    const wAfterScale = node.getBoundingClientRect().width;
+    const hAfterScale = node.getBoundingClientRect().height;
+    let top = false;
+    let left = false;
+    if (wAfterScale < vw - 10) {
+      top = 0;
+      left = (vw - wAfterScale) / 2;
+    }
+    if (hAfterScale < vh - 10) {
+      top = (vh - hAfterScale) / 2;
+    }
+    if (top !== false || left !== false) {
+      node.style.position = 'absolute';
+      node.style.top = top + 'px';
+      node.style.left = left + 'px';
+    }
+    node.scaled = true;
+  }
+  resize();
+  window.addEventListener('resize', resize);
+}
+
+window.addEventListener('load', init);
